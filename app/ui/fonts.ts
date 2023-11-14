3fa9270e0c79acf07459afc6d5861fee937e0352
Mauro Nigrele <mauro.nigrele@gmail.com>
2023-11-14 14:08:43 -0300

    Import

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Roboto_Mono, Lusitana } from 'next/font/google';
+export const inter = Inter({ subsets: ['latin'] });
+export const robotom = Roboto_Mono({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
+  
